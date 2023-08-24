7608d247b51c28ac8ec14199e28944e582a49cd7
aswin_setiadi <aswin.setiadi@gmail.com>
2023-08-24 12:33:15 +0800

    add q1-2 unittest for long time

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -229,3 +229,9 @@ class TestVirusMap:
         n = 5
         matrix = [[2, 0, 0, 2, 2], [0, 2, 2, 2, 2], [2, 0, 0, 2, 2]]
         assert VirusMap(m, n, matrix).solve() == "0"
+
+    def test_long_time(self):
+        m = 3
+        n = 5
+        matrix = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 0], [1, 1, 1, 1, 2]]
+        assert VirusMap(m, n, matrix).solve() == "10"
