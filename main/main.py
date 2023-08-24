fe33d04ea929c3c221c461f4cb5068ba85ee832d
aswin_setiadi <aswin.setiadi@gmail.com>
2023-08-24 10:35:08 +0800

    update class and method description

--- main/main.py
+++ main/main.py
@@ -22,8 +22,14 @@ else:
 
 class Hotel:
     """
-    Hotel class that represents the hotel system.
-    There are always 5 rooms for each floor.
+    Hotel class that represents the hotel system. There must be always 5 rooms for each floor.
+
+    Parameters:
+        m_floors <int>: number of floor
+
+        rooms_w_status <List[List[str]]>: floor matrix with each room set with user defined status (optional, default to all Available)
+
+    Acceptable statuses are "Available", "Occupied", "Vacant", "Repair".
     """
 
     def __init__(
@@ -50,6 +56,7 @@ class Hotel:
                 self._rooms.append([Room(f"{i+1}{j}") for j in HOTEL_COLUMNS])
 
     def assign_room(self) -> Union[str, None]:
+        """Assign Available room nearest to the hotel entrance. Will return room number or None if no Available room."""
         for i in range(self.floor_count):
             if i % 2:
                 for j in reversed(self._rooms[i]):
@@ -63,7 +70,8 @@ class Hotel:
                         return j.number
         return None
 
-    def list_available_rooms(self):
+    def list_available_rooms(self) -> List:
+        """Will return a list of Available room number in order from closest to furthest room from the hotel entrance."""
         avail_rooms = []
         for i in range(self.floor_count):
             if i % 2:
@@ -78,7 +86,15 @@ class Hotel:
 
 
 class Room:
-    """Room class"""
+    """
+    Room class.
+
+    Parameters:
+
+        number <str>: room number, format is {i}{j} where i is floor number and j is room column (A-E).
+
+        status <str>: room status, acceptable values are "Available", "Occupied", "Vacant", "Repair".
+    """
 
     def __init__(self, number: str, status: str = ROOM_STATUSES[0]) -> None:
         if status not in ROOM_STATUSES:
@@ -95,30 +111,35 @@ class Room:
         return self._status
 
     def check_in(self) -> bool:
+        """Set room status from Available to Occupied, returns True if success."""
         if self._status != ROOM_STATUSES[0]:
             raise CheckInException()
         self._status = ROOM_STATUSES[1]
         return True
 
     def check_out(self) -> bool:
+        """Set room status from Occupied to Vacant, returns True if success."""
         if self._status != ROOM_STATUSES[1]:
             raise CheckOutException()
         self._status = ROOM_STATUSES[2]
         return True
 
     def clean(self) -> bool:
+        """Set room status from Vacant to Available, returns True if success."""
         if self._status != ROOM_STATUSES[2]:
             raise CleanException()
         self._status = ROOM_STATUSES[0]
         return True
 
     def repair(self) -> bool:
+        """Set room status from Vacant to Repair, returns True if success."""
         if self._status != ROOM_STATUSES[2]:
             raise RepairException()
         self._status = ROOM_STATUSES[3]
         return True
 
     def repaired(self) -> bool:
+        """Set room status from Repair to Vacant, returns True if success."""
         if self._status != ROOM_STATUSES[3]:
             raise RepairedException()
         self._status = ROOM_STATUSES[2]
